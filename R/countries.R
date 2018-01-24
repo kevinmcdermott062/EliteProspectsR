2902e525c432befda57638449cc340ee0cd16b76
Phil OSx <bulsinkp@gmail.com>
2018-01-23 21:49:18 -0500

    Add some country & transfer work

--- R/countries.R
+++ R/countries.R
@@ -11,3 +11,21 @@
 listCountries <- function(filters=NULL, sorts=NULL, fields=NULL, limit=0, offset=0){
   return(simple_list(path="countries/", filters=filters, sorts=sorts, fields=fields, limit=limit, offset=offset))
 }
+
+#' Get a specific Country Entry
+#'
+#' @param id The transfer entry ID to get
+#' @param filters Any filters to apply
+#' @param sorts Any sorting to apply to results
+#' @param fields Specific fields to retrieve
+#' @param limit Maximum number of results returned
+#' @param offset Offset of returned results
+#'
+#' @return The retired number entry from the API
+#' @export
+getCountry <- function(id=NULL, filters=NULL, sorts=NULL, fields=NULL, limit=0, offset=0){
+  if(!is.numeric(id)){
+    stop("Please enter a country id to get")
+  }
+  return(simple_get(path="countries/", id=id, filters=filters, sorts=sorts, fields=fields, limit=limit, offset=offset))
+}
--- R/transfers.R
+++ R/transfers.R
@@ -11,3 +11,21 @@
 listTransfers <- function(filters=NULL, sorts=NULL, fields=NULL, limit=0, offset=0){
   return(simple_list(path="transfers/", filters=filters, sorts=sorts, fields=fields, limit=limit, offset=offset))
 }
+
+#' Get a specific Transfer Entry
+#'
+#' @param id The transfer entry ID to get
+#' @param filters Any filters to apply
+#' @param sorts Any sorting to apply to results
+#' @param fields Specific fields to retrieve
+#' @param limit Maximum number of results returned
+#' @param offset Offset of returned results
+#'
+#' @return The retired number entry from the API
+#' @export
+getTransfer <- function(id=NULL, filters=NULL, sorts=NULL, fields=NULL, limit=0, offset=0){
+  if(!is.numeric(id)){
+    stop("Please enter a transfer id to get")
+  }
+  return(simple_get(path="transfers/", id=id, filters=filters, sorts=sorts, fields=fields, limit=limit, offset=offset))
+}
